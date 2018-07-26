970c3d9ab7a26b8a8f70de5bcff1a8ee5b0feea2
bw-bmbarga <bmbarga@brandwatch.com>
2018-07-26 17:35:16 +0200

    Add table creation script

---
+++ sql/create_tokens_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tokens (
+	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	token VARCHAR(40) NOT NULL,
+	userid INT UNSIGNED,
+	expirationdate DATETIME NOT NULL,
+	creationdate DATETIME NOT NULL,
+	postmethod enum('all', 'own', 'none') NOT NULL DEFAULT 'none',
+	patchmethod enum('all', 'own', 'none') NOT NULL DEFAULT 'none',
+	getmethod enum('all', 'own', 'none') NOT NULL DEFAULT 'own',
+	delmethod enum('all', 'own', 'none') NOT NULL DEFAULT 'none',
+	requests INT UNSIGNED,
+	PRIMARY KEY (id),
+	UNIQUE KEY ix_token (token)
+) ENGINE=InnoDB AUTO_INCREMENT=0 CHARSET=utf8;
