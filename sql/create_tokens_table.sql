ec1c9d97b9d81917d344d75babb9370200ef343d
bw-bmbarga <bmbarga@brandwatch.com>
2018-08-05 10:27:21 +0200

    Remove authorization from token table

--- sql/create_tokens_table.sql
+++ sql/create_tokens_table.sql
@@ -4,12 +4,7 @@ CREATE TABLE tokens (
 	userid INT UNSIGNED,
 	expirationdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	creationdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	postmethod enum('all', 'own', 'none') NOT NULL DEFAULT 'none',
-	patchmethod enum('all', 'own', 'none') NOT NULL DEFAULT 'own',
-	getmethod enum('all', 'own', 'none') NOT NULL DEFAULT 'own',
-	delmethod enum('all', 'own', 'none') NOT NULL DEFAULT 'none',
 	requests INT UNSIGNED,
 	PRIMARY KEY (id),
 	UNIQUE KEY ix_token (token),
-	UNIQUE KEY ix_userid (userid)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 CHARSET=utf8;
